7765f9f66ebdd9a126c609720bcc6cf3c7152c49
Michael Connell <63263149+michaelconnell10@users.noreply.github.com>
2025-03-07 19:55:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
+red_bull = {"Verstappen": 0, "Perez": 0}
+mercedes = {"Russell": 0, "Hamilton": 0}
+mclaren = {"Norris": 0, "Piastri": 0}
+ferrari = {"Leclerc": 0, "Sainz": 0, "Bearman": 0}
+alpine = {"Gasly": 0, "Ocon": 0, "Doohan": 0}
+# I refuse to call them "Visa Cash App RB Racing Team" and am calling them Alpha Tauri
+alpha_tauri = {"Tsunoda": 0, "Ricciardo": 0, "Lawson": 0}
+aston_martin = {"Alonso": 0, "Stroll": 0}
+williams = {"Albon": 0, "Sargent": 0, "Colapinto": 0}
+haas = {"Hulkenberg": 0, "Magnussen": 0, "Bearman": 0}
+sauber = {"Bottas": 0, "Zhou": 0}
